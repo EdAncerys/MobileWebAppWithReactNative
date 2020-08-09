efa44cfbb7b58c12e068ebe5149ab0d7d10c7f00
Ed <lookatemail@gmail.com>
2020-08-09 12:44:35 +0100

    Able to seperate platform specific settings

--- app/components/AppText.js
+++ app/components/AppText.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import { StyleSheet, Text, Platform } from 'react-native';
 
+import colors from '../config/colors';
+
 export default function AppText({ children }) {
   return <Text style={styles.text}>{children}</Text>;
 }
 
 const styles = StyleSheet.create({
   text: {
-    fontSize: 18,
-    fontFamily: Platform.OS === 'android' ? 'Roboto' : 'Avenir',
+    color: 'tomato',
+    ...Platform.select({
+      ios: {
+        color: colors.primary,
+        fontSize: 20,
+        fontFamily: 'Avenir',
+      },
+      android: {
+        color: colors.black,
+        fontSize: 20,
+        fontFamily: 'Roboto',
+      },
+    }),
   },
 });
