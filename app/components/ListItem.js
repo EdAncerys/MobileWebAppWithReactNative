5d1bec6911ef17b74158dfc88939f093f9ff1ad1
Ed <lookatemail@gmail.com>
2020-08-09 18:43:22 +0100

    Add user component

---
+++ app/components/ListItem.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import colors from '../config/colors';
+
+export default function ListItem({ title, subTitle, image }) {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.image} source={image} />
+      <View>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.subTitle}>{subTitle}</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+  },
+  image: {
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    marginRight: 10,
+  },
+  subTitle: {
+    color: colors.gray,
+  },
+  title: {
+    fontWeight: '500',
+  },
+});
--- app/components/ListingDetailsScreen.js
+++ app/components/ListingDetailsScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet, View, Image } from 'react-native';
 import AppText from './AppText';
 import colors from '../config/colors';
+import ListItem from './ListItem';
 
 export default function ListingDetailsScreen() {
   return (
@@ -10,6 +11,13 @@ export default function ListingDetailsScreen() {
       <View style={styles.detailsContainer}>
         <AppText style={styles.title}>Bike for Sale</AppText>
         <AppText style={styles.price}>$100</AppText>
+        <View style={styles.userContainer}>
+          <ListItem
+            image={require('../assets/logo-red.png')}
+            title="Ed Ancerys"
+            subTitle="5 Listings"
+          />
+        </View>
       </View>
     </View>
   );
@@ -23,6 +31,9 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 24,
     fontWeight: '500',
+  },
+  userContainer: {
+    marginVertical: 40,
   },
   price: {
     color: colors.secondary,
