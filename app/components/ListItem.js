359120300eb5d4aa3dec5ed6c40dadd59865eb40
Ed <lookatemail@gmail.com>
2020-08-10 15:34:43 +0100

    Add visual hilight on touchable component

--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableHighlight,
+} from 'react-native';
 import colors from '../config/colors';
 
 export default function ListItem({ title, subTitle, image }) {
   return (
-    <View style={styles.container}>
-      <Image style={styles.image} source={image} />
-      <View>
-        <Text style={styles.title}>{title}</Text>
-        <Text style={styles.subTitle}>{subTitle}</Text>
+    <TouchableHighlight
+      underlayColor={colors.lightGray}
+      onPress={() => console.log()}
+    >
+      <View style={styles.container}>
+        <Image style={styles.image} source={image} />
+        <View>
+          <Text style={styles.title}>{title}</Text>
+          <Text style={styles.subTitle}>{subTitle}</Text>
+        </View>
       </View>
-    </View>
+    </TouchableHighlight>
   );
 }
 
