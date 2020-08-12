2f817156430258b11e1dde2a7d68de74929a9812
Ed <lookatemail@gmail.com>
2020-08-12 17:20:04 +0100

    Add reusable icon component

--- App.js
+++ App.js
@@ -14,9 +14,15 @@ import AppText from './app/components/AppText';
 import AppButton from './app/components/AppButton';
 import ListingDetailsScreen from './app/components/ListingDetailsScreen';
 import MessagesScreen from './app/screens/MessagesScreen';
+import Screen from './app/components/Screen';
+import Icon from './app/components/Icon';
 
 export default function App() {
-  return <MessagesScreen />;
+  return (
+    <Screen>
+      <Icon name="email" size={50} backgroundColor="red" iconColor="white" />
+    </Screen>
+  );
 }
 
 const styles = StyleSheet.create({
