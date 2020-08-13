f0cbbc60be7b27f35d548798fa76cde188340415
Ed <lookatemail@gmail.com>
2020-08-13 18:25:37 +0100

    Able to export all form components via index.js file

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -3,10 +3,8 @@ import { StyleSheet, Image } from 'react-native';
 
 import * as Yup from 'yup';
 
-import AppForm from '../components/forms/AppForm';
-import AppFormField from '../components/forms/AppFormField';
 import Screen from '../components/Screen';
-import SubmitButton from '../components/forms/SubmitButton';
+import { AppForm, AppFormField, SubmitButton } from '../components/forms';
 
 const validationSchema = Yup.object().shape({
   email: Yup.string().required().email().label('Email'),
