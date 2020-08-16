5ed461810da2823c887b8063e1014b75082d59f6
Ed <lookatemail@gmail.com>
2020-08-16 18:17:11 +0100

    Build ImageImput component

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import * as ImagePicker from 'expo-image-picker';
 import Screen from './app/components/Screen';
 import { Button, Image } from 'react-native';
+import ImageInput from './app/components/ImageInput';
 
 export default function App() {
   const [imageUri, setImageUri] = useState();
@@ -28,6 +29,7 @@ export default function App() {
     <Screen>
       <Button title="Select Image" onPress={selectImage} />
       <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
+      <ImageInput imageUri={imageUri} />
     </Screen>
   );
 }
