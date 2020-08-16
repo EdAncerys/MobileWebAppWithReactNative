a180944f6e9f7e79ff597bf2f21de3eea77832b4
Ed <lookatemail@gmail.com>
2020-08-16 19:06:30 +0100

    Add image inputList component: able to add images to list

--- App.js
+++ App.js
@@ -2,34 +2,25 @@ import React, { useEffect, useState } from 'react';
 import * as ImagePicker from 'expo-image-picker';
 import Screen from './app/components/Screen';
 import { Button, Image } from 'react-native';
-import ImageInput from './app/components/ImageInput';
+import ImageInputList from './app/components/ImageInputList';
 
 export default function App() {
-  const [imageUri, setImageUri] = useState();
+  const [imageUris, setImageUris] = useState([]);
 
-  const requestPermision = async () => {
-    const { granted } = await ImagePicker.requestCameraRollPermissionsAsync();
-    if (!granted) alert('You need to enable permission to access the library');
+  const handleRemove = (uri) => {
+    setImageUris(imageUris.filter((imageUri) => imageUri !== uri));
   };
 
-  useEffect(() => {
-    requestPermision();
-  }, []);
-
-  const selectImage = async () => {
-    try {
-      const { uri, cancelled } = await ImagePicker.launchImageLibraryAsync();
-      if (!cancelled) setImageUri(uri);
-    } catch (error) {
-      console.log('error');
-    }
+  const handleAdd = (uri) => {
+    setImageUris([...imageUris, uri]);
   };
 
   return (
     <Screen>
-      <ImageInput
-        imageUri={imageUri}
-        onChangeImage={(uri) => setImageUri(uri)}
+      <ImageInputList
+        imageUris={imageUris}
+        onAddImage={handleAdd}
+        onRemoveImage={handleRemove}
       />
     </Screen>
   );
