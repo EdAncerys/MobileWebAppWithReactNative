21a1c99e24da052090f4b07608c42082d126bfcb
Ed <lookatemail@gmail.com>
2020-08-17 15:09:28 +0100

    Implement navigation for Login & register screens

--- App.js
+++ App.js
@@ -1,77 +1,13 @@
 import React from 'react';
-import { Text, View, Button } from 'react-native';
 
-import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import Screen from './app/components/Screen';
+import AuthNavigator from './app/navigation/AuthNavigator';
 
-const Tweets = ({}) => {
-  return (
-    <Screen>
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Home!</Text>
-      </View>
-    </Screen>
-  );
-};
-
-const TweetDetails = ({ navigation, route }) => {
-  return (
-    <Screen>
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Settings!</Text>
-      </View>
-    </Screen>
-  );
-};
-
-function HomeScreen() {
-  return (
-    <Screen style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </Screen>
-  );
-}
-
-const Stack = createStackNavigator();
-const StackNavigator = () => {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="Tweets" component={Tweets} />
-      <Stack.Screen name="TweetDetails" component={TweetDetails} />
-    </Stack.Navigator>
-  );
-};
-
-const Account = () => {
-  return (
-    <Screen style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Account</Text>
-    </Screen>
-  );
-};
-
-const Tab = createBottomTabNavigator();
-const TabNavigator = () => {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="Feed" component={Tweets} />
-      <Tab.Screen name="Details" component={TweetDetails} />
-      <Tab.Screen name="Account" component={Account} />
-    </Tab.Navigator>
-  );
-};
-
-export default function App({ route }) {
+export default function App() {
   return (
     <NavigationContainer>
-      {/* <Stack.Navigator>
-        <Stack.Screen name="Home" component={StackNavigator} />
-      </Stack.Navigator> */}
-
-      <TabNavigator />
+      <AuthNavigator />
     </NavigationContainer>
   );
 }
