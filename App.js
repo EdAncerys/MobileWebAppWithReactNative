273906af104588b838e1177928f10d3c6d099690
Ed <lookatemail@gmail.com>
2020-08-17 13:11:46 +0100

    Passing params to routes

--- App.js
+++ App.js
@@ -1,7 +1,57 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { Text, Button } from 'react-native';
 
-import ListingEditScreen from './app/screens/ListingEditScreen';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Screen from './app/components/Screen';
+
+const Tweets = ({ navigation }) => {
+  return (
+    <Screen>
+      <Text>Tweets</Text>
+      <Button
+        title="View Tweet"
+        onPress={() => navigation.push('TweetDetails', { id: 1, name: 'Name' })}
+      />
+    </Screen>
+  );
+};
+
+const TweetDetails = ({ route }) => {
+  return (
+    <Screen>
+      <Text>
+        Tweet Details {route.params.id} {route.params.name}
+      </Text>
+    </Screen>
+  );
+};
+
+function HomeScreen() {
+  return (
+    <Screen style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </Screen>
+  );
+}
+
+const Stack = createStackNavigator();
+const StackNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Tweets" component={Tweets} />
+      <Stack.Screen name="TweetDetails" component={TweetDetails} />
+    </Stack.Navigator>
+  );
+};
 
 export default function App() {
-  return <ListingEditScreen />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={StackNavigator} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
